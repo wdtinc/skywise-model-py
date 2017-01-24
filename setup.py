dd7d1593dd70bae1b4a36a2b3af95851e105fbb8
spandalraju <shravya@wdtinc.com>
2017-01-24 22:21:20 +0000

    Updated forecast args schema

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='skywise-model',
-    version='0.0.2',
+    version='0.0.3',
     package_data={'': ['README.md']},
     packages=['skywisemodel'],
     install_requires=[
