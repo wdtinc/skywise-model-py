8b5e6858ee4c6da1da4bc81f3ed527a1cbed5ff2
Justin Stewart <jstewart@wdtinc.com>
2017-02-02 09:40:09 -0600

    Version bump.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='skywise-model',
-    version='0.0.6',
+    version='0.0.7',
     package_data={'': ['README.md']},
     packages=['skywisemodel'],
     install_requires=[
